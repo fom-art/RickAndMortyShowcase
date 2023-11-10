c4aee6a5398bba3ea65db510f1e13e0688ee27d6
anfo-code <artem29040506000@gmail.com>
2023-11-10 01:08:08 +0100

    debug getCharacters and filterCharactersByName so that they were showing all of  the characters instead of characters from only the first page

--- app/src/main/java/com/example/rickyandmortyshowcase/database/remote/data/client/ApolloRickAndMortyShowcaseClient.kt
+++ app/src/main/java/com/example/rickyandmortyshowcase/database/remote/data/client/ApolloRickAndMortyShowcaseClient.kt
@@ -1,5 +1,6 @@
 package com.example.rickyandmortyshowcase.database.remote.data.client
 
+import android.util.Log
 import com.apollographql.apollo3.ApolloClient
 import com.example.CharacterQuery
 import com.example.CharactersQuery
@@ -15,13 +16,22 @@ class ApolloRickAndMortyShowcaseClient(
     private val rickAndMortyShowcaseClient: ApolloClient
 ) : RickAndMortyShowcaseClient {
     override suspend fun getCharacters(): List<CharacterSimple> {
-        return rickAndMortyShowcaseClient.query(CharactersQuery())
-            .execute()
-            .data
-            ?.characters
-            ?.results
-            ?.map { it!!.toCharacterSimple() }
-            ?: emptyList()
+        var result = emptyList<CharacterSimple>()
+        val pagesAmount = rickAndMortyShowcaseClient.query(CharactersQuery(1))
+            .execute().data!!.characters!!.info!!.pages ?: 0
+        var currentPage = 1
+        while (currentPage in 1..pagesAmount) {
+            result = result + (rickAndMortyShowcaseClient
+                .query(CharactersQuery(currentPage))
+                .execute()
+                .data
+                ?.characters
+                ?.results
+                ?.map { it!!.toCharacterSimple() }
+                ?: emptyList())
+            currentPage++
+        }
+        return result
     }
 
     override suspend fun getCharacterDetails(id: String): CharacterDetailed? {
@@ -30,17 +40,23 @@ class ApolloRickAndMortyShowcaseClient(
             .data
             ?.character
             ?.toCharacterDetailed()
-
     }
 
     override suspend fun getCharactersByName(name: String): List<CharacterSimple> {
-        return rickAndMortyShowcaseClient.query(FilterCharactersByNameQuery(name))
-            .execute()
-            .data
-            ?.characters
-            ?.results
-            ?.map { it!!.toCharacterSimple() }
-            ?: emptyList()
+        var result = emptyList<CharacterSimple>()
+        val pagesAmount = rickAndMortyShowcaseClient.query(FilterCharactersByNameQuery(page = 1, character_name = name))
+            .execute().data!!.characters!!.info!!.pages ?: 0
+        var currentPage = 1
+        while (currentPage in 1..pagesAmount) {
+            result = result + (rickAndMortyShowcaseClient.query(FilterCharactersByNameQuery(character_name = name, page = currentPage))
+                .execute()
+                .data
+                ?.characters
+                ?.results
+                ?.map { it!!.toCharacterSimple() }
+                ?: emptyList())
+            currentPage++
+        }
+        return result
     }
-
 }
